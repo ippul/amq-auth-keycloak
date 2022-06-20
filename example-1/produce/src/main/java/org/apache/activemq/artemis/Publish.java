737db499bae78301416773aa3d0cdf194172807f
Claudio Luppi <cluppi@redhat.com>
2022-06-20 18:11:47 +0100

    new folders structure

--- example-1/produce/src/main/java/org/apache/activemq/artemis/Publish.java
+++ example-1/produce/src/main/java/org/apache/activemq/artemis/Publish.java
@@ -6,26 +6,18 @@ import java.util.UUID;
 
 public class Publish {
     public static void main(String[] args) throws Exception {
-        for(int count = 0; count<1000; count ++) {
-            publishMessageAs(
-                    "queue1",
-                    "ippul",
-                    "Pa$$w0rd",
-                    "Test JMS Message " + UUID.randomUUID().toString()
-            );
-            Thread.sleep(1000l);
-        }
-    }
-    public static void publishMessageAs(final String destinationName, final String username, final String password, final String messageBody) throws JMSException {
-        final ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://ex-aao-hdls-svc:61616", username, password);
+        final ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://ex-aao-hdls-svc:61616", "ippul", "Pa$$w0rd");
         final Connection connection = connectionFactory.createConnection();
         connection.start();
         final Session session  = connection.createSession();
-        final Destination destination = session.createQueue(destinationName);
+        final Destination destination = session.createQueue("queue1");
         final MessageProducer producer = session.createProducer(destination);
-        final TextMessage message = session.createTextMessage(messageBody);
-        System.out.println("Sending: " + messageBody);
-        producer.send(message);
+        for(int count = 0; count<10_000_000; count ++) {
+            final TextMessage message = session.createTextMessage("Test JMS Message " + UUID.randomUUID().toString());
+            System.out.println("Sending: " + message);
+            producer.send(message);
+            Thread.sleep(10l);
+        }
         session.close();
         connection.close();
     }
